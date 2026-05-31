ffa68629ec34a50a775d11ba53a81a6bc3ea05fe
mpatange-spec <mpatange50237@ucumberlands.edu>
2026-06-01 00:41:00 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        
+        //main logic
+        appBar: AppBar(
+          title: const Text("Hello world Application"),
+        ),
+        body: const Center(
+          child: Text(
+            "Hello World",
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
